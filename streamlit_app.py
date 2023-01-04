dfca225095a57da50ac8bfe2eafb550581a629a2
adamiksz7 <54018762+adamiksz7@users.noreply.github.com>
2023-01-04 20:12:48 +0000

    Update streamlit_app.py
    closed sf connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,7 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
@@ -67,6 +68,7 @@ addfruit_choice = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(addfruit_choice)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
 streamlit.stop()
