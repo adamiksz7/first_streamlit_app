a2e404fec2f545af6193318b0ad060d02d2f98e3
adamiksz7 <54018762+adamiksz7@users.noreply.github.com>
2023-01-04 19:43:50 +0000

    Update streamlit_app.py
    stop streamlit so rows don't get inserted many times

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
